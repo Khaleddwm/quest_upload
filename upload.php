1a6a1584e7d49bba3e693325b0de9744ee97083a
khaleddwm <benk.devweb@gmail.com>
2020-04-27 09:19:05 +0200

    remise au propre, dernier commit

--- upload.php
+++ upload.php
@@ -9,40 +9,32 @@ $uploadDir = 'uploads/';
 $filename = $_FILES['avatar']['name'];
 // taille de fichier accéptés
 $taille_maxi = 1000000;
-
 // Extension accéptées
 $extensions = ['jpg', 'jpeg', 'png', 'gif'];
 
 // Début des vérifications de sécurité...
 for($i=0; $i < count($_FILES['avatar']['name']); $i++) {
-    
     // obtenir uniquement l'extension (sans le point)
     $extension = pathinfo($_FILES['avatar']['name'][$i], PATHINFO_EXTENSION);
     // taile du fichier uploadé
     $taille = filesize($_FILES['avatar']['tmp_name'][$i]);
-    
     // Si l'extension n'est pas dans le tableau
     if(!in_array($extension, $extensions)) { 
         $error =  'Votre fichier "' . $filename[$i] . '" n\'est pas conforme' .'<br>' .
         'Vous devez uploader un fichier de type png, gif, jpg, txt ou doc...';
     }
-    
     // Si la taille est trop volumineuse
     if($taille > $taille_maxi) {
         $error = 'Votre fichier "' . $filename[$i] . '" n\'est pas conforme' .'<br>' .
         'Le fichier est trop gros...';
     }
-    
     //S'il n'y a pas d'erreur, on upload
     if(!isset($erreur)) { 
-        
         // A unique name is concatenated with a dot and the $extention avec l'extension récupérée
         $filenameUniq = uniqid() . '.' .$extension;
-        
         // Si la fonction renvoie TRUE, c'est que ça a fonctionné...
         if(move_uploaded_file($_FILES['avatar']['tmp_name'][$i], $uploadDir . $filenameUniq)) {
             echo 'Upload du fichier "' . $filename[$i] . '" effectué avec succès !' .'<br>';
-        
         // Sinon (la fonction renvoie FALSE).
         } else {   
             echo 'Echec de l\'upload "' . $filename[$i] . '" !'  .'<br>';
@@ -56,13 +48,9 @@ for($i=0; $i < count($_FILES['avatar']['name']); $i++) {
 $file = new FilesystemIterator('uploads');
 
 // Option Delete
-
-if(unlink($_POST['submit'])) {
+if(unlink($_POST['delete'])) {
     header('Location:upload.php');
 }
-
-
-
 
 ?>
 
@@ -101,7 +89,7 @@ if(unlink($_POST['submit'])) {
             echo '<img style="max-width:220px; max-height:150px;" src="uploads/' . $fileinfo->getFilename() . '" />';
             echo '<figcaption style="background-color:#222; color:#fff; font:italic smaller sans-serif; padding:3px; text-align:center;">' . $fileinfo->getFilename() . '</figcaption>';
             if (file_exists($fileinfo)) {
-                echo '<button class="btn btn-warning" type="submit"  name="submit" value="' . $fileinfo . '">Delete</button>';
+                echo '<button class="btn btn-warning" type="submit"  name="delete" value="' . $fileinfo . '">Delete</button>';
                 echo '</figure>';
             } 
         }
