c34c32b12c088abf3a7cbe6e6b361b3313f8e52f
khaleddwm <benk.devweb@gmail.com>
2020-05-02 14:43:27 +0200

    info error debug rectified

--- upload.php
+++ upload.php
@@ -8,48 +8,51 @@ $uploadDir = 'uploads/';
 // retour du nom du fichier
 $filename = $_FILES['avatar']['name'];
 // taille de fichier accéptés
-$taille_maxi = 1000000;
+$sizeMax = 1048576;
 // Extension accéptées
 $extensions = ['jpg', 'jpeg', 'png', 'gif'];
 
 // Début des vérifications de sécurité...
-for($i = 0; $i < count($_FILES['avatar']['name']); $i++) {
-    // obtenir uniquement l'extension (sans le point)
-    $extension = pathinfo($_FILES['avatar']['name'][$i], PATHINFO_EXTENSION);
-    // taile du fichier uploadé
-    $taille = filesize($_FILES['avatar']['tmp_name'][$i]);
-    // Si l'extension n'est pas dans le tableau
-    if(!in_array($extension, $extensions)) { 
-        $error =  'Votre fichier "' . $filename[$i] . '" n\'est pas conforme' .'<br>' .
-        'Vous devez uploader un fichier de type png, gif, jpg, txt ou doc...';
-    }
-    // Si la taille est trop volumineuse
-    if($taille > $taille_maxi) {
-        $error = 'Votre fichier "' . $filename[$i] . '" n\'est pas conforme' .'<br>' .
-        'Le fichier est trop gros...';
-    }
-    //S'il n'y a pas d'erreur, on upload
-    if(!isset($error)) { 
-        // A unique name is concatenated with a dot and the $extention avec l'extension récupérée
-        $filenameUniq = uniqid() . '.' .$extension;
-        // Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-        if(move_uploaded_file($_FILES['avatar']['tmp_name'][$i], $uploadDir . $filenameUniq)) {
-            echo 'Upload du fichier "' . $filename[$i] . '" effectué avec succès !' .'<br>';
-        // Sinon (la fonction renvoie FALSE).
-        } else {   
-            echo 'Echec de l\'upload "' . $filename[$i] . '" !'  .'<br>';
+if (isset($_POST['upload'])) {
+    for($i = 0; $i < count($_FILES['avatar']['name']); $i++) {
+        // obtenir uniquement l'extension (sans le point)
+        $extension = pathinfo($_FILES['avatar']['name'][$i], PATHINFO_EXTENSION);
+        // taile du fichier uploadé
+        $size = filesize($_FILES['avatar']['tmp_name'][$i]);
+        // Si l'extension n'est pas dans le tableau
+        if(!in_array($extension, $extensions)) {
+            $message[] =  'Votre fichier "' . $filename[$i] . '" n\'est pas conforme' . '<br>' .
+            'Vous devez uploader un fichier de type png, gif, jpg, txt ou doc...';
         }
-    } else {
-        echo $error;
+        // Si la taille est trop volumineuse
+        if($size > $sizeMax) {
+            $message[] = 'Votre fichier "' . $filename[$i] . '" n\'est pas conforme' . '<br>' .
+            'Le fichier est trop gros...';
+        }
+        //S'il n'y a pas d'erreur, on upload
+        if(!isset($error)) { 
+            // A unique name is concatenated with a dot and the $extention avec l'extension récupérée
+            $filenameUniq = uniqid() . '.' .$extension;
+            // Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+            if(move_uploaded_file($_FILES['avatar']['tmp_name'][$i], $uploadDir . $filenameUniq)) {
+                $message[] = 'Upload du fichier "' . $filename[$i] . '" effectué avec succès !' .'<br>';
+            // Sinon (la fonction renvoie FALSE).
+            } else {   
+                $message[] = 'Echec de l\'upload "' . $filename[$i] . '" !'  .'<br>';
+            }
+        } else {
+            return $message;
+        }
     }
 }
 
 // liste image upload
 $file = new FilesystemIterator('uploads');
-
 // Option Delete
-if(unlink($_POST['delete'])) {
+if (isset($_POST['delete'])) {
+    unlink($_POST['delete']);
     header('Location:upload.php');
+    exit();
 }
 
 ?>
@@ -62,22 +65,32 @@ if(unlink($_POST['delete'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Quest Upluoad</title>
 </head>
-<body style="background-color:darkblue; color:white">
+
+<body style="background-color:darkblue/; color:white/; margin-bottom:100px">
     <div>
-        <h1 style="background-color:black; text-align:center">Quest Upload</h1>
+        <h1 style="background-color:black; color:white; text-align:center">Quest Upload</h1>
         <h2 style="text-align:center; background-color:darkturquoise; color:yellow" for="imageUpload">Upload image</h2></br>
     </div>
     <div class="d-flex justify-content-center">
         <form style="display:flex; flex-direction:column" class="" method="POST" action="upload.php" enctype="multipart/form-data">
             <!-- On limite le fichier à 100Ko -->
             <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
-            <input class="btn btn-light" type="file" name="avatar[]" multiple="multiple" /></br>
-            <button class="btn btn-success" type="submit" >Upload</button>
+            <input class="btn btn-light" type="file" name="avatar[]" multiple="multiple" required/></br>
+            <button class="btn btn-success" type="submit" name="upload">Upload</button>
             <a class="btn btn-light" style="margin-top:20px; color:red" href="upload.php">Rafraichir</a>
-            
         </form>
     </div>
+    <br><h3 style="text-align:center">
+        <?php 
+        if (isset($message)) {
+            foreach ($message as $value) {
+            echo $value;
+            }
+        }
+        ?>
+        </h3><br>
     <div>
+    
         <br><h2 style="text-align:center; background-color:darkturquoise; color:yellow">Avatar en stock :</h2><br>
     </div>
     <div style="">
@@ -92,6 +105,7 @@ if(unlink($_POST['delete'])) {
                 echo '</figure>';
             } 
         }
+        
         ?>
         </form>
     </div>        
